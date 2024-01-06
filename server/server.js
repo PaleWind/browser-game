17aa7129c0d10714b835968765fea0956abed332
aoi <palu83xoo@gmail.com>
2024-01-06 04:17:41 +0000

    adding folders for structure

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('.')); // Serve static files from the current directory
+app.use(express.static('./game')); // Serve static files from the current directory
 
 app.listen(port, () => {
   console.log(`Game server running at http://localhost:${port}`);
