6f35edef314190229f0c1072858dc17e8128432e
aoi <palu83xoo@gmail.com>
2024-01-07 21:30:27 +0000

    player controls

--- game/Game.js
+++ game/Game.js
@@ -1,15 +1,34 @@
 import Player from "./Player.js"
 
 class Game { 
+
     constructor(canvas) {
+        //properties
         this.canvas = canvas
         this.width = this.canvas.width
         this.height = this.canvas.height 
         this.player = new Player(this)
+        this.keys = []
+
+        //player input listeners
+        window.addEventListener('keydown', (e) => {
+            if (this.keys.indexOf(e.key) === -1) {
+                this.keys.push(e.key)
+            }
+        })
+
+        window.addEventListener('keyup', (e) => {
+            const index = this.keys.indexOf(e.key)
+            if (index > -1) this.keys.splice(index, 1)
+        })
+
     }
+
     render(ctx) {
         this.player.render(ctx)
+        this.player.update()
     }
+
 }
 
 export default Game
--- game/Player.js
+++ game/Player.js
@@ -4,14 +4,28 @@ class Player {
         this.game = game
         this.width = 100
         this.height = 100 
-        this.x = 100
-        this.y = 433
+        this.x = this.game.width * 0.5 - this.width * 0.5
+        this.y = this.game.height * 0.5 - this.height * 0.5
+        this.speed = 5
     }
 
     render(ctx) {
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 
+    update() {
+        this.handlePlayerInput()
+    }
+
+    handlePlayerInput() {
+        const moveLeft = this.game.keys.includes('ArrowLeft') ? -1 : 0;
+        const moveRight = this.game.keys.includes('ArrowRight') ? 1 : 0;
+        const moveUp = this.game.keys.includes('ArrowUp') ? -1 : 0;
+        const moveDown = this.game.keys.includes('ArrowDown') ? 1 : 0;
+
+        this.x += (moveRight + moveLeft) * this.speed;
+        this.y += (moveDown + moveUp) * this.speed;
+    }
 }
 
 export default Player
