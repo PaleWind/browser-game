3a61d380fdeb54adb8e085055fde22f186b0958a
aoi <palu83xoo@gmail.com>
2024-01-09 00:11:42 +0000

    projectiles

--- game/Game.js
+++ game/Game.js
@@ -1,34 +1,60 @@
 import Player from "./Player.js"
+import Projectile from "./Projectile.js"
 
 class Game { 
 
-    constructor(canvas) {
+    constructor(ctx) {
         //properties
-        this.canvas = canvas
+        this.canvas = ctx
         this.width = this.canvas.width
         this.height = this.canvas.height 
         this.player = new Player(this)
         this.keys = []
+        this.projectilePoolSize = 10
+        this.projectilePool = []
 
+        this.bindInputcontrols()
+        this.loadProjectilePool()
+    }
+
+    render(ctx) {
+        this.player.update()
+        this.player.render(ctx)
+        this.projectilePool.forEach(projectile => {
+            projectile.update()
+            projectile.render(ctx)
+        })
+    }
+
+    bindInputcontrols() {
         //player input listeners
         window.addEventListener('keydown', (e) => {
+            console.log(e.key)
             if (this.keys.indexOf(e.key) === -1) {
                 this.keys.push(e.key)
             }
+            if (e.key === '1') this.player.shoot()
+            if (e.key === '2') this.player.shoot()
         })
 
         window.addEventListener('keyup', (e) => {
             const index = this.keys.indexOf(e.key)
             if (index > -1) this.keys.splice(index, 1)
         })
-
+    }
+ 
+    loadProjectilePool() {
+        for (let i = 0; i < this.projectilePoolSize; i++) {
+            this.projectilePool.push(new Projectile())
+        }
     }
 
-    render(ctx) {
-        this.player.render(ctx)
-        this.player.update()
+    getAvailableProjectile() {
+        for (let i = 0; i < this.projectilePoolSize; i++) {
+            if (this.projectilePool[i].inPool) return this.projectilePool[i]
+        }
     }
-
+ 
 }
 
 export default Game
--- game/Player.js
+++ game/Player.js
@@ -4,8 +4,10 @@ class Player {
         this.game = game
         this.width = 100
         this.height = 100 
-        this.x = this.game.width * 0.5 - this.width * 0.5
-        this.y = this.game.height * 0.5 - this.height * 0.5
+        this.mX = this.width * 0.5 // middle of the player model x-axis
+        this.mY = this.height * 0.5 // middle of the player model y-axis
+        this.x = this.game.width * 0.5 - this.mX
+        this.y = this.game.height * 0.5 - this.mY
         this.speed = 5
     }
 
@@ -14,10 +16,15 @@ class Player {
     }
 
     update() {
-        this.handlePlayerInput()
+        this.#handlePlayerMovement()
     }
 
-    handlePlayerInput() {
+    shoot() {
+       const projectile = this.game.getAvailableProjectile()
+       if (projectile) projectile.start(this.x + this.mX, this.y) 
+    }
+
+    #handlePlayerMovement() {
         const moveLeft = this.game.keys.includes('ArrowLeft') ? -1 : 0;
         const moveRight = this.game.keys.includes('ArrowRight') ? 1 : 0;
         const moveUp = this.game.keys.includes('ArrowUp') ? -1 : 0;
@@ -26,6 +33,7 @@ class Player {
         this.x += (moveRight + moveLeft) * this.speed;
         this.y += (moveDown + moveUp) * this.speed;
     }
+
 }
 
 export default Player
